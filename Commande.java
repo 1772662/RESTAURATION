eb72414d4bc793e22570008fbc502da3eb61d94a
amina <1772662@cmontmorency.qc.ca>
2019-02-13 14:04:55 -0500

    Declaration varaible et ajout contruteurs

--- Commande.java
+++ Commande.java
@@ -1,6 +1,19 @@
 
 public class Commande {
+	private Client client;
+	private String nomPlat;
+	private int quantite;
 
+	public Commande() {
+
+	}
+
+	public Commande(Client client, String  nomPlat, int quantite) {
+		this.client = client;
+		this.nomPlat = nomPlat;
+		this.quantite = quantite;
+
+	}
 
 	
 }
